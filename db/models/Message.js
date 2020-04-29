7ba4a59431e3f8cdb9382bc12ee0a1efd1f39acb
Juan Sebastián Bravo <js.bravo@uniandes.edu.co>
2020-04-29 14:24:21 -0500

    Added API routes for users and matches.

--- db/models/Message.js
+++ db/models/Message.js
@@ -1,9 +1,12 @@
 module.exports.Message = class {
+<<<<<<< HEAD
   /**
    * Creates a message.
    * @param {Player} player Player that created the message.
    * @param {String} message Message.
    */
+=======
+>>>>>>> 74802ec... Added API routes for users and matches.
   constructor(player, message) {
     this.player = player;
     this.message = message;
