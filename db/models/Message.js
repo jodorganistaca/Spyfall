cbf546881be67cc7ce35f7ec4f115929aabd9683
Juan Sebastián Bravo <js.bravo@uniandes.edu.co>
2020-04-30 15:44:50 -0500

    Added player JSON creation endpoint.
    Improved docs.

--- db/models/Message.js
+++ db/models/Message.js
@@ -1,12 +1,9 @@
 module.exports.Message = class {
-<<<<<<< HEAD
   /**
    * Creates a message.
    * @param {Player} player Player that created the message.
    * @param {String} message Message.
    */
-=======
->>>>>>> 74802ec... Added API routes for users and matches.
   constructor(player, message) {
     this.player = player;
     this.message = message;
